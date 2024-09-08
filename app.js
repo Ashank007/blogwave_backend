21d600aba6b41296dff8e965ca12b116fb430179
Ashank Gupta <142603084+Ashank007@users.noreply.github.com>
2024-09-08 18:21:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,8 +11,7 @@ connectDb();
 app.use(cors({
     origin:[process.env.FRONTEND_URL,"http://localhost:3000"],
     credentials:true,
-    allowedHeaders: ['Authorization', 'Content-Type'],
-    exposedHeaders: ['Authorization'] 
+    methods:["GET","DELETE","POST","PUT"]
 }))
 app.use(express.json())
 app.use(cookieParser())
